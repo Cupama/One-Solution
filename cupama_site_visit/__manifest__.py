128bcfc9c2b4392cfbbded68af820ca7f95d763f
MaximilienNaina <maximilien@nexources.com>
2026-03-03 16:13:29 +0300

    [ADD]new module: site_visit

---
+++ cupama_site_visit/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Cupama Site Visit Sheet',
+    'version': '16.0.1.0.0',
+    'category': 'Sales',
+    'summary': 'Site Visit Sheet for SPC Flooring Installation',
+    'description': """
+        Module to manage site visit sheets for SPC flooring installation.
+        Accessible via a smart button on Sale Orders and Quotations.
+    """,
+    'author': 'A.Maximilien',
+    'depends': ['sale_management'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/site_visit_sheet_views.xml',
+        'views/sale_order_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
---
+++ cupama_site_visit/models/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+from . import site_visit_sheet
+from . import sale_order
